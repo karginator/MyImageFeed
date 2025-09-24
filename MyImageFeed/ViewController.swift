588224b939e1d6e1f08289c6eb6ac7f2dba45064
Maksim Kargin <maksim.kargin@gmail.com>
2025-09-24 15:43:06 +0700

    Initial Commit

---
+++ MyImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyImageFeed
+//
+//  Created by Maksim Kargin on 24.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
