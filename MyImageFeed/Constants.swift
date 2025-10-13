9e9293e4aae3e403f87e2dd14d1524a6036726a6
Maksim Kargin <maksim.kargin@gmail.com>
2025-10-13 19:57:44 +0700

    Add Constants

---
+++ MyImageFeed/Constants.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+enum Constants {
+    static let accessKae = "qnCthhA4b0SBzVWg-W7SnjhVgzV_DqX8oFfPN8FUeK8"
+    static let secretKey = "YUO-J1QcPA8m8Yfo90i7j6zR6GAiqpljEafRKF7DwFQ"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_user"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
